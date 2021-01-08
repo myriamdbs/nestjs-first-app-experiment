a282104f55196c3cc5b9dbe7536163e428600021
Myriam Dubois <myriam.dubois@skema.edu>
2021-01-08 17:29:50 +0100

    add business logic with item controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ItemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
